5e2c80103f9385a0639cd87e21442011e27a5813
Simon Cruanes <simon.cruanes.2007@m4x.org>
2021-12-10 21:17:18 -0500

    iobuf: add Iobuf.Chain, to store multiple buffers
    this is useful as a low-allocation storage for intermediate output
    (before compression)

--- src/iobuf.ml
+++ src/iobuf.ml
@@ -16,8 +16,8 @@ let[@inline] get self i =
 let[@inline] clear self = self.i <- 0; self.after_last <- 0
 let[@inline] write_cap self = cap self - self.after_last
 
-let[@inline] write_char self c =
-  if self.after_last >= cap self then invalid_arg "Iobuf.write_char";
+let[@inline] write_byte self c =
+  if self.after_last >= cap self then invalid_arg "Iobuf.write_byte";
   Bytes.set self.b self.after_last c;
   self.after_last <- 1 + self.after_last
 
@@ -48,4 +48,65 @@ module Pool = struct
   let recycle self b : unit =
     clear b;
     self.bufs <- b :: self.bufs
+end
+
+module Chain = struct
+  type t = {
+    mutable first: iobuf;
+    mutable last: iobuf;
+    q: iobuf Queue.t;
+    pool: Pool.t;
+  }
+
+  let create ~pool () : t =
+    let buf = Pool.alloc pool in
+    let q = Queue.create () in
+    Queue.push buf q;
+    { first=buf; last=buf; q; pool }
+
+  let[@inline] first self = self.first
+  let[@inline] last self = self.last
+
+  let[@inline] iter self ~f = Queue.iter f self.q
+
+  let len self =
+    let n = ref 0 in
+    iter self ~f:(fun buf -> n := !n + len buf);
+    !n
+
+  let dealloc self =
+    iter self ~f:(fun buf -> clear buf; Pool.recycle self.pool buf);
+    Queue.clear self.q
+
+  let contents self =
+    let total_len = len self in
+    let result = Bytes.make total_len '\x00' in
+    let i = ref 0 in
+    iter self ~f:(fun buf ->
+        let len_buf = buf.after_last - buf.i in
+        Bytes.blit buf.b buf.i result !i len_buf;
+        i := !i + len_buf);
+    assert (!i = total_len);
+    Bytes.unsafe_to_string result
+
+  let[@inline never] add_buf_ self =
+    let buf = Pool.alloc self.pool in
+    Queue.push buf self.q;
+    self.last <- buf
+
+  let[@inline] write_byte self c =
+    if write_cap self.last = 0 then add_buf_ self;
+    write_byte self.last c
+
+  (* write slice, possibly into separate buffers *)
+  let[@unroll 1] rec write_slice_rec_ self b i len =
+    if len>0 then (
+      if write_cap self.last = 0 then add_buf_ self;
+      let len' = min len (write_cap self.last) in
+      assert (len' > 0);
+      write_slice self.last b i len';
+      if len>len' then write_slice_rec_ self b (i+len') (len-len')
+    )
+
+  let write_slice = write_slice_rec_
 end
--- src/iobuf.mli
+++ src/iobuf.mli
@@ -21,15 +21,17 @@ val get : t -> int -> char
 val write_cap : t -> int
 (** How much can we write *)
 
-val write_char : t -> char -> unit
-(** Write a single char. Assumes [write_cap buf > 0] *)
+val write_byte : t -> char -> unit
+(** Write a single char.
+    @raise Invalid_argument if [write_cap buf = 0] *)
 
 val write_slice : t -> bytes -> int -> int -> unit
 (** [write_slice buf b i len] writes a slice of [b] to [buf].
-    Assumes [len <= write_cap buf]. *)
+    @raise Invalid_argument if [len > write_cap buf]. *)
 
 val consume : t -> int -> unit
 
+(** Pool of buffers, to reuse them once they're consumed *)
 module Pool : sig
   type t
 
@@ -38,4 +40,31 @@ module Pool : sig
   val alloc : t -> iobuf
 
   val recycle : t -> iobuf -> unit
+end
+
+(** A chain of buffers stringed together *)
+module Chain : sig
+  type t
+
+  val create : pool:Pool.t -> unit -> t
+
+  val first : t -> iobuf
+  val last : t -> iobuf
+
+  val len : t -> int
+  (** Sum of length of buffers *)
+
+  val iter : t -> f:(iobuf -> unit) -> unit
+
+  val dealloc : t -> unit
+  (** Release all buffers. Do not use after calling that. *)
+
+  val write_byte : t -> char -> unit
+  (** Write a single char at the end. *)
+
+  val write_slice : t -> bytes -> int -> int -> unit
+  (** Write a slice at the end. *)
+
+  val contents : t -> string
+  (** Turn whole contents into a single string *)
 end
