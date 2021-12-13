5d6bc2ba1667d9a0fbe19a8daf54d4f36576899a
Simon Cruanes <simon.cruanes.2007@m4x.org>
2021-12-13 16:43:57 -0500

    improve tests, add a test for `to_array_list`

--- tests/employee_test.ml
+++ tests/employee_test.ml
@@ -2,6 +2,14 @@
 module E = Employee
 let spf = Printf.sprintf
 
+type decode_into =
+  | Seq
+  | List_of_arrays
+
+let string_of_dec_into = function
+  | Seq -> "seq"
+  | List_of_arrays -> "list-of-arrays"
+
 let seq_len seq =
   let rec loop n seq = match seq() with
     | Seq.Nil -> n
@@ -58,39 +66,58 @@ let test_file ~codec_name ~n file : unit =
   encode_to_out ~codec_name ~n out;
   ()
 
-let decode_from (input:Avro.Input.t) : int =
+let decode_from ~decode_into (input:Avro.Input.t) : int =
   let open Avro.Obj_container_file.Decode in
   let dec = make input ~read:E.read in
-  let seq = to_seq dec in
-  seq_len seq
+  match decode_into with
+  | Seq ->
+    let seq = to_seq dec in
+    seq_len seq
+  | List_of_arrays ->
+    let l = to_array_list dec in
+    List.fold_left (fun n a -> n + Array.length a) 0 l
 
-let test_str_read (str:string ) : unit =
-  Printf.printf "reading back from string…\n%!";
-  let n = decode_from (Avro.Input.of_string str) in
+let test_str_read ~decode_into (str:string ) : unit =
+  Printf.printf "reading back from string into %s…\n%!" (string_of_dec_into decode_into);
+  let n = decode_from ~decode_into (Avro.Input.of_string str) in
   Printf.printf "read back %d rows\n%!" n;
   ()
 
-let test_file_read file : unit =
-  Printf.printf "reading back from file %S…\n%!" file;
+let test_file_read ~decode_into file : unit =
+  Printf.printf "reading back from file %S into %s…\n%!"
+    file (string_of_dec_into decode_into);
   Avro.Input.with_file file @@ fun input ->
-  let n = decode_from input in
+  let n = decode_from ~decode_into input in
   Printf.printf "read back %d rows\n%!" n;
   ()
 
 let () =
   Printexc.record_backtrace true;
+  let out = ref "" in
+  let codec_name = ref "null" in
+  let n = ref 50_000 in
+  let dec_into = ref Seq in
 
-  let codec_name = (try Sys.getenv "CODEC" with _ -> "null") in
-  let n = (try Sys.getenv "N" |> int_of_string with _ -> 50_000) in
+  let opts = [
+    "-o", Arg.Set_string out, " output file";
+    "-n", Arg.Set_int n, " number of items";
+    "--codec", Arg.Set_string codec_name, " compression codec";
+    "--seq", Arg.Unit (fun()->dec_into := Seq), " decode as seq";
+    "--list-of-array", Arg.Unit (fun()->dec_into := List_of_arrays), " decode as list of arrays";
+  ] |> Arg.align in
+  Arg.parse opts (fun _ -> failwith "no positional arg") "";
 
-  begin match Sys.getenv_opt "OUT" with
-    | None ->
+  let codec_name = !codec_name in
+  let n = !n in
+  let decode_into = !dec_into in
+  begin match !out with
+    | "" ->
       let str = test_str ~codec_name ~n () in
-      test_str_read str;
-    | Some file ->
+      test_str_read ~decode_into str;
+    | file ->
       Printf.printf "write to file %S\n%!" file;
       test_file ~codec_name ~n file;
-      test_file_read file;
+      test_file_read ~decode_into file;
   end;
   ()
 
